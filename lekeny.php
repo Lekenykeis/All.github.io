229be9549f8922525e2ff78b0c91a4f6f52d667f
Your Name <clintonkeis799@gmail.com>
2018-02-22 12:35:24 +0300

    first commit

---
+++ lekeny.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form>
+		<input type="text" name="person">
+		<button>submit</button>
+	</form>
+	<?php
+	echo "<h1>class pearl Hello World</h1><br>";
+	echo str_word_count("My name is kontire junior and i love CODING...if i was to make a decision right now i would be starting a new project of making the most amazing website of all time...But why me...<br>")
+	
+</body>
+</html>
